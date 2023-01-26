eaeef83d3966e907cef78be6673df2738f40ff46
Александр Востриков <aptimict11@gmail.com>
2023-01-27 02:34:13 +0300

    Initial Commit

---
+++ iosadv-homeworks-swiftUI/iosadv_homeworks_swiftUIApp.swift
@@ -0,0 +1,19 @@
+//
+//
+// iosadv_homeworks_swiftUIApp.swift
+// iosadv-homeworks-swiftUI
+//
+// Created by Александр Востриков
+//
+    
+
+import SwiftUI
+
+@main
+struct iosadv_homeworks_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
